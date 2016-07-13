260f855a7f55be2c177a1e072774834e27e5e118
Kopan Artem <artemkopan@gmail.com>
2016-07-13 21:53:08 +0300

    add shadow; add fragment toolbar;

--- app/src/main/java/com/artemkopan/baseproject/sample/MainActivity.java
+++ app/src/main/java/com/artemkopan/baseproject/sample/MainActivity.java
@@ -1,13 +1,70 @@
 package com.artemkopan.baseproject.sample;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View mRevealView;
+    private View mRevealBackgroundView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.appbar);
+//
+        setSupportActionBar(mToolbar);
+
+
+        mRevealView = findViewById(R.id.reveal);
+        mRevealBackgroundView = findViewById(R.id.revealBackground);
+
+        ToggleButton toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean on = ((ToggleButton) v).isChecked();
+
+                if (on) {
+                    animateAppAndStatusBar(R.color.colorPrimary, R.color.colorAccent);
+                } else {
+                    animateAppAndStatusBar(R.color.colorAccent, R.color.colorPrimary);
+                }
+            }
+        });
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void animateAppAndStatusBar(int fromColor, final int toColor) {
+        Animator animator = ViewAnimationUtils.createCircularReveal(
+                mRevealView,
+                mToolbar.getWidth() / 2,
+                mToolbar.getHeight() / 2, 0,
+                mToolbar.getWidth() / 2);
+
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                mRevealView.setBackgroundColor(getResources().getColor(toColor));
+            }
+        });
+
+        mRevealBackgroundView.setBackgroundColor(getResources().getColor(fromColor));
+        animator.setStartDelay(200);
+        animator.setDuration(125);
+        animator.start();
+        mRevealView.setVisibility(View.VISIBLE);
     }
 }
--- base_project/src/main/java/com/artemkopan/baseproject/fragment/BaseFragment.java
+++ base_project/src/main/java/com/artemkopan/baseproject/fragment/BaseFragment.java
@@ -1,27 +1,38 @@
 package com.artemkopan.baseproject.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.artemkopan.baseproject.activity.BaseActivity;
+import com.artemkopan.baseproject.helper.Log;
 import com.artemkopan.baseproject.rx.Lifecycle;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import rx.subjects.PublishSubject;
 
+import static butterknife.ButterKnife.findById;
 
 public abstract class BaseFragment extends Fragment {
 
-
+    private static final String TAG = "BaseFragment";
     public PublishSubject<Lifecycle> mPublishSubject = PublishSubject.create();
     @SuppressWarnings("SpellCheckingInspection")
     protected Unbinder mUnbinder;
+
+    @Nullable
+    protected ActionBar mActionBar;
+    @Nullable
+    protected Toolbar mToolbar;
 
     @Nullable
     @Override
@@ -51,6 +62,44 @@ public abstract class BaseFragment extends Fragment {
     public void onDestroy() {
         mPublishSubject.onNext(Lifecycle.ON_DESTROY);
         super.onDestroy();
+    }
+
+    public void onToolbarInit(@IdRes int toolbarId, boolean fromActivity) {
+        if (fromActivity && getActivity() != null) {
+            mToolbar = findById(getActivity(), toolbarId);
+        } else if (fromActivity && getView() != null) {
+            mToolbar = findById(getView(), toolbarId);
+        } else {
+            Log.e(TAG,
+                    "From Activity: " + fromActivity +
+                            "\nActivity: " + getActivity() +
+                            "\nView: " + getView());
+        }
+        setActionBar(mToolbar);
+    }
+
+    public void onToolbarSetTitle(String title) {
+        if (mToolbar != null) {
+            mToolbar.setTitle(title);
+        }
+    }
+
+    public void onToolbarShowBackBtn(boolean show) {
+        if (mActionBar == null) {
+            return;
+        }
+        if (show) {
+            mActionBar.setDisplayHomeAsUpEnabled(true);
+        } else {
+            mActionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
+    private void setActionBar(Toolbar toolbar) {
+        if (toolbar != null && getActivity() != null && getActivity() instanceof AppCompatActivity) {
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+            mActionBar = ((AppCompatActivity) getActivity()).getSupportActionBar();
+        }
     }
 
     /**
