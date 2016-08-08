aba6a4dea3091cd962ee13d3a6f3c3a052e5776d
Artem Kopan <artemkopan@gmail.com>
2016-08-08 17:34:57 +0300

    fix message dialog

--- base_project/src/main/java/com/artemkopan/baseproject/dialog/DialogController.java
+++ base_project/src/main/java/com/artemkopan/baseproject/dialog/DialogController.java
@@ -2,24 +2,14 @@ package com.artemkopan.baseproject.dialog;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import com.artemkopan.baseproject.R;
 import com.artemkopan.baseproject.activity.BaseActivity;
 import com.artemkopan.baseproject.fragment.BaseDialogFragment;
 import com.artemkopan.baseproject.fragment.BaseFragment;
-
-import static butterknife.ButterKnife.findById;
 
 @SuppressWarnings("WeakerAccess")
 public class DialogController {
@@ -124,79 +114,14 @@ public class DialogController {
             return null;
         }
 
-        if (mMessageDialog == null) {
-            mMessageDialog = messageFragment;
-        } else if ((mMessageDialog.isShowing())) {
-            return null;
+        if (mMessageDialog != null) {
+            mMessageDialog.dismiss();
         }
+        mMessageDialog = messageFragment;
 
         mMessageDialog.show(activity.getSupportFragmentManager());
 
         return mMessageDialog;
-    }
-
-    @SuppressWarnings("unused")
-    public static class MessageDialog extends BaseDialogFragment {
-
-        private static final String KEY_TITLE = "TITLE";
-        private static final String KEY_DESCRIPTION = "DESCRIPTION";
-
-        private Runnable mOnDismissAction;
-
-        public static MessageDialog newInstance(String title, String description) {
-            Bundle args = new Bundle();
-            args.putString(KEY_TITLE, title);
-            args.putString(KEY_DESCRIPTION, description);
-            MessageDialog fragment = new MessageDialog();
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public void onBaseDialogSize(Window window) {
-            window.setLayout(
-                    (int) (getContext().getResources().getDisplayMetrics().widthPixels * 0.8),
-                    WindowManager.LayoutParams.WRAP_CONTENT);
-        }
-
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.base_dialog_message, container, false);
-            String title = getArguments().getString(KEY_TITLE);
-            String description = getArguments().getString(KEY_DESCRIPTION);
-
-            TextView titleView = findById(view, R.id.base_dialog_message_title);
-            titleView.setText(title);
-            TextView descriptionView = findById(view, R.id.base_dialog_message_description);
-            descriptionView.setText(description);
-
-            findById(view, R.id.base_dialog_message_close).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    dismissAllowingStateLoss();
-                }
-            });
-
-            return view;
-        }
-
-        @Override
-        public int onCreateInflateView() {
-            return 0;
-        }
-
-        public void setOnDismissAction(Runnable onDismissAction) {
-            mOnDismissAction = onDismissAction;
-        }
-
-        @Override
-        public void onDismiss(DialogInterface dialog) {
-            if (mOnDismissAction != null) {
-                mOnDismissAction.run();
-            }
-            super.onDismiss(dialog);
-        }
     }
 }
 
---
+++ base_project/src/main/java/com/artemkopan/baseproject/dialog/MessageDialog.java
@@ -0,0 +1,83 @@
+package com.artemkopan.baseproject.dialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.artemkopan.baseproject.R;
+import com.artemkopan.baseproject.fragment.BaseDialogFragment;
+
+import static butterknife.ButterKnife.findById;
+
+/**
+ * Created by artem on 08.08.16.
+ */
+@SuppressWarnings("unused")
+public class MessageDialog extends BaseDialogFragment {
+
+    private static final String KEY_TITLE = "TITLE";
+    private static final String KEY_DESCRIPTION = "DESCRIPTION";
+
+    private Runnable mOnDismissAction;
+
+    public static MessageDialog newInstance(String title, String description) {
+        Bundle args = new Bundle();
+        args.putString(KEY_TITLE, title);
+        args.putString(KEY_DESCRIPTION, description);
+        MessageDialog fragment = new MessageDialog();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onBaseDialogSize(Window window) {
+        window.setLayout(
+                (int) (getContext().getResources().getDisplayMetrics().widthPixels * 0.8),
+                WindowManager.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.base_dialog_message, container, false);
+        String title = getArguments().getString(KEY_TITLE);
+        String description = getArguments().getString(KEY_DESCRIPTION);
+
+        TextView titleView = findById(view, R.id.base_dialog_message_title);
+        titleView.setText(title);
+        TextView descriptionView = findById(view, R.id.base_dialog_message_description);
+        descriptionView.setText(description);
+
+        findById(view, R.id.base_dialog_message_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismissAllowingStateLoss();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public int onCreateInflateView() {
+        return 0;
+    }
+
+    public void setOnDismissAction(Runnable onDismissAction) {
+        mOnDismissAction = onDismissAction;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        if (mOnDismissAction != null) {
+            mOnDismissAction.run();
+        }
+        super.onDismiss(dialog);
+    }
+}
