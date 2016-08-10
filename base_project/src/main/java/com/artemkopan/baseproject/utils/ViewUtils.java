c4226a1b4357eeb5bfec395f1dc4d774d60a1d40
Artem Kopan <artemkopan@gmail.com>
2016-08-10 18:00:53 +0300

    add view behavior position; add new methods in ViewUtils;

--- base_project/src/main/java/com/artemkopan/baseproject/utils/ViewUtils.java
+++ base_project/src/main/java/com/artemkopan/baseproject/utils/ViewUtils.java
@@ -31,4 +31,47 @@ public class ViewUtils {
         return null;
     }
 
+    /**
+     * Get relative value of X position from root View;
+     * Default parentView is System DecorView;
+     *
+     * @param view currentView;
+     * @return Relative X value;
+     */
+    public static float getRelativeX(View view, @Nullable View parentView) {
+        if (view.getParent() == (parentView == null ? view.getRootView() : parentView))
+            return view.getX();
+        else
+            return view.getX() + getRelativeX((View) view.getParent(), parentView);
+    }
+
+    /**
+     * Get relative value of Y position from root View;
+     * Default parentView is System DecorView;
+     *
+     * @param view currentView
+     * @return Relative Y value;
+     */
+    public static float getRelativeY(View view, @Nullable View parentView) {
+        if (view.getParent() == (parentView == null ? view.getRootView() : parentView))
+            return view.getY();
+        else
+            return view.getY() + getRelativeY((View) view.getParent(), parentView);
+    }
+
+    /**
+     * Get center of view and plus X position;
+     */
+    public static float getCenterViewXPos(View view) {
+        int width = view.getWidth();
+        return view.getX() + (width > 0 ? width / 2 : 0);
+    }
+
+    /**
+     * Get center of view and plus Y position;
+     */
+    public static float getCenterViewYPos(View view) {
+        int height = view.getHeight();
+        return view.getY() + (height > 0 ? height / 2 : 0);
+    }
 }
