c26b06bc462ed3e76b88e87118cef605f92154a2
artemkopan <artemkopan@gmail.com>
2016-11-20 14:17:46 +0200

    add basic recycler holder; add view pager transorms;

---
+++ base_project/src/main/java/com/artemkopan/baseproject/recycler/holder/BasicHolder.java
@@ -0,0 +1,15 @@
+package com.artemkopan.baseproject.recycler.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created for jabrool
+ * by Kopan Artem on 31.10.2016.
+ */
+
+public class BasicHolder extends RecyclerView.ViewHolder {
+    public BasicHolder(View itemView) {
+        super(itemView);
+    }
+}
